4e1db57301f5d5f158f5ed6cc1e52b6bc11cc27e
Takao Mizuno <takaomizuno@takaoMacBook-Air.local>
2023-01-07 21:42:11 +1000

    implementing fetch news

---
+++ news.js
@@ -0,0 +1,9 @@
+class News {
+    constructor(title, desicription, url) {
+        this.title = title;
+        this.desicription = desicription;
+        this.url = url;
+    }
+}
+
+module.exports = News

1b220bb4246f22dbf79b5e6118f5348608930bd1
Takao Mizuno <takaomizuno@takaoMacBook-Air.local>
2023-01-07 18:17:27 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const line = require("@line/bot-sdk")
+const PORT = process.env.PORT || 3000;
+
+const config = {
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET
+}
+
+const app = express();
+app.get('/', (req, res) => res.send("Hello World!"));
+app.post('/webhook', line.middleware(config), (req, res) => {
+    Promise
+        .all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result));
+});
+
+const client = new line.Client(config);
+function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        return Promise.resolve(null);
+    }
+
+    return client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: event.message.text
+    });
+}
+
+app.listen(PORT);
